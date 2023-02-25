d4228a1778cac229408312b6c5caf32939d8a152
Veaceslav Vlas <vlasslavic@gmail.com>
2023-02-24 22:09:16 -0500

    Re-design + Logo + Name

--- Deliverables/Phase 1/dashboard.py
+++ Deliverables/Phase 1/dashboard.py
@@ -9,34 +9,62 @@ GPIO.setwarnings(False)
 GPIO.setup(21, GPIO.OUT)
 
 
-app = Dash(__name__)
-img = html.Img(src=app.get_asset_url('lightbulb_off.png'),width='100px', height='100px')
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP,dbc.icons.FONT_AWESOME]
+)
+
+img = html.I(className="fa-regular fa-lightbulb w-100", style={'font-size': '10rem'})
+
+# Web page title
+app.title = 'CasaConnect'
 
 
-app.layout = html.Div(children=[
-    dcc.Store(id='local-led-status', storage_type='local'),
-    html.H1(children='My IoT Dashboard', style={'display': 'flex', 'justify-content':'center'} ),
-    html.Div(id='led-box', children=[
-        html.H1(children=True, style={'text-align': 'center'}),
-        html.Button(img, id='led-image', n_clicks = 0)
-    ]),
-], style={ 'justify-content':'center'})
+nav = html.Div(children='Log In', style={'text-align': 'center'})
+         
 
+header = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink(nav), style={'text-align': 'end'})
+    ],
+    brand=[html.Div(id='Logo-full', children=[
+         html.Img(src=app.get_asset_url('logo_full.png'), width='400rem',  className="d-flex justify-content-center"),
+        ], className="d-flex justify-content-center")],
+    brand_href="#",
+    color="light",
+    dark=False,
+    sticky='top'
+)
+
+body = html.Div([
+            header, 
+            dbc.Container([
+                dbc.Row([
+                      html.Button(img, className='btn btn-light mt-3 w-25 py-4', type="button", id='led-image', n_clicks = 0),
+                        ]),
+                                          
+                ]),
+            # ])     
+        ], className="body"
+        );
+
+app.layout = html.Div(id="theme-switch-div", children=[
+    body
+    ]);
 
 @app.callback(Output('led-image', 'children'),
               Input('led-image', 'n_clicks'))
 def update_output(n_clicks):
     if n_clicks % 2 == 1:
         GPIO.output(21, GPIO.HIGH)
-        img = html.Img(src=app.get_asset_url('ledLamp_on.png'), width='100px', height='100px')
+        img = html.I(className="fa-solid fa-lightbulb", style={'font-size': '10rem', 'color': '#E4742E', 'filter': 'drop-shadow(0 0 50px #eac86c)'})
         return img
     else:
         GPIO.output(21, GPIO.LOW)
-        img = html.Img(src=app.get_asset_url('ledLamp_off.png'),width='100px', height='100px')
+        img = html.I(className="fa-regular fa-lightbulb", style={'font-size': '10rem', 'color':'#1b8bd1'})
         return img
 
 
 def main():
-    app.run_server(debug=True, host='localhost', port=8052)
+    app.run_server(debug=True, host='localhost', port=8050)
 
 main()

949b0775ab2970dcc1d4aa988d22b9ec17ea5ed3
Veaceslav Vlas <vlasslavic@gmail.com>
2023-02-27 20:37:58 -0500

    Updated Design + Created Separate Button

--- Deliverables/Phase 1/dashboard.py
+++ Deliverables/Phase 1/dashboard.py
@@ -2,6 +2,7 @@ import dash.dependencies
 import dash_daq as daq
 from dash import html, Input, Output, dcc, Dash
 import dash_bootstrap_components as dbc
+
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
@@ -9,15 +10,12 @@ GPIO.setwarnings(False)
 GPIO.setup(21, GPIO.OUT)
 
 
-app = dash.Dash(
+app = dash.Dash(__name__,
     external_stylesheets=[dbc.themes.BOOTSTRAP,dbc.icons.FONT_AWESOME]
 )
 
-img = html.I(className="fa-regular fa-lightbulb w-100", style={'font-size': '10rem'})
-
 # Web page title
 app.title = 'CasaConnect'
-
 
 nav = html.Div(children='Log In', style={'text-align': 'center'})
          
@@ -27,37 +25,65 @@ header = dbc.NavbarSimple(
         dbc.NavItem(dbc.NavLink(nav), style={'text-align': 'end'})
     ],
     brand=[html.Div(id='Logo-full', children=[
-         html.Img(src=app.get_asset_url('logo_full.png'), width='400rem',  className="d-flex justify-content-center"),
-        ], className="d-flex justify-content-center")],
-    brand_href="#",
+         html.Img(src=app.get_asset_url('logo_full.png'), width='400rem',  ),
+        ], className="w-100")],
+    brand_href="/",
     color="light",
     dark=False,
-    sticky='top'
+    sticky='top', 
+    className="d-flex justify-content-between shadow-lg "
 )
 
-body = html.Div([
-            header, 
-            dbc.Container([
+
+body = html.Main(children=[
+        dbc.Container(children=[
+             html.H1(id='', className='container mt-3', children='Dashboard:'),
+             html.Hr(children=[]),
                 dbc.Row([
-                      html.Button(img, className='btn btn-light mt-3 w-25 py-4', type="button", id='led-image', n_clicks = 0),
-                        ]),
+                    html.Div(children=[
+                        html.Div(children='Lighting:', className='display-6 ms-3'),
+                        html.Div(children=[
+                            html.Div(children=[], className='d-flex justify-content-center mt-4 mb-3', id='led-power-button-result'),
+                            daq.PowerButton(
+                                id='led-power-button',
+                                className='mb-2',
+                                color='#ffe100',
+                                size=75,
+                                on=False
+                            ),    
+                        ],className="card rounded-4 w-100 bg-light py-2 px-4 m-3 shadow-sm"),
+                    ], className='w-25'),
+                ], className='mb-3'),
                                           
-                ]),
+            ], className=" rounded-4 card m-4 shadow")
             # ])     
-        ], className="body"
-        );
+        ], className="flex-shrink-0 d-flex justify-content-center"
+    );
+
+footer = html.Footer([
+            html.Div(className="container text-center ", children=[
+                  html.Small(children=['2023 © CasaConnect by ', html.Span(children='GREEN', className='text-success', style={'filter': 'drop-shadow(0 0 80px green)'}), ' Team'])
+            ]),
+            ],id='sticky-footer', className="fixed-bottom py-4 bg-dark text-white-50 ")
+
+
 
 app.layout = html.Div(id="theme-switch-div", children=[
-    body
+    header,
+    body,
+    footer
     ]);
 
-@app.callback(Output('led-image', 'children'),
-              Input('led-image', 'n_clicks'))
-def update_output(n_clicks):
-    if n_clicks % 2 == 1:
+@app.callback(
+    Output('led-power-button-result', 'children'),
+    Input('led-power-button', 'on')
+)
+def update_output(on):
+    if on:
         GPIO.output(21, GPIO.HIGH)
-        img = html.I(className="fa-solid fa-lightbulb", style={'font-size': '10rem', 'color': '#E4742E', 'filter': 'drop-shadow(0 0 50px #eac86c)'})
+        img = html.I(className="fa-solid fa-lightbulb", style={'font-size': '10rem', 'color': '#ffe100', 'filter': 'drop-shadow(0 0 50px #eac86c)'})
         return img
+            
     else:
         GPIO.output(21, GPIO.LOW)
         img = html.I(className="fa-regular fa-lightbulb", style={'font-size': '10rem', 'color':'#1b8bd1'})

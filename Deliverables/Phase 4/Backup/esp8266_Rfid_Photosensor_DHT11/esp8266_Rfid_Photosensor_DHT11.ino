15513e7baedb088fb6ce2b311d56f609d1a88233
Veaceslav Vlas <vlasslavic@gmail.com>
2023-04-27 16:09:08 -0400

    Phase 4

---
+++ Deliverables/Phase 4/Backup/esp8266_Rfid_Photosensor_DHT11/esp8266_Rfid_Photosensor_DHT11.ino
@@ -0,0 +1,233 @@
+ #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Wire.h>  
+#include <LiquidCrystal_I2C.h> 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define SS_PIN D8
+#define RST_PIN D0
+#define DHTPIN D4
+#define DHTTYPE DHT11
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key;
+// Init array that will store new NUID
+byte nuidPICC[4];
+
+const char* ssid = "TP-LINK_6BAC";
+const char* password = "95706801";
+const char* mqtt_server = "192.168.0.159";
+
+//const char* ssid = "iPhone";
+//const char* password = "boss1234";
+//const char* mqtt_server = "172.20.10.2";
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient vanieriot;
+PubSubClient client(vanieriot);
+
+const int pin1 = A0;
+int sensorVal;
+const int ANALOG_READ_PIN = 36; 
+const int RESOLUTION = 12;
+long now = millis();
+long lastMeasure = 0;
+
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP-8266 IP address: ");
+  Serial.println(WiFi.localIP());
+}
+void callback(String topic, byte *message, unsigned int length)
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messagein;
+
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)message[i]);
+    messagein += (char)message[i];
+  }
+}
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("vanieriot"))
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 3 seconds");
+      // Wait 5 seconds before retrying
+      delay(3000);
+    }
+  }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+  lcd.init();   // initializing the LCD
+  lcd.backlight();
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522
+  Serial.println();
+  Serial.print(F("Reader :"));
+  rfid.PCD_DumpVersionToSerial();
+  for (byte i = 0; i < 6; i++) {
+      key.keyByte[i] = 0xFF;
+  }
+  Serial.println();
+  Serial.println(F("This code scan the MIFARE Classic NUID."));
+  Serial.print(F("Using the following key:"));
+  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+
+  dht.begin();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  pinMode(pin1, INPUT);
+}
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  if (!client.loop())
+    client.connect("vanieriot");
+
+  now = millis();
+  if (now - lastMeasure > 5000)
+  {
+    lastMeasure = now;
+    float t = analogRead(pin1);
+    float tem = dht.readTemperature();
+    float humi = dht.readHumidity();
+    lcd.setCursor(0, 0);
+    Serial.print("Temperature is: ");
+    Serial.print(tem);
+    Serial.print(" *C; Humidity is: ");
+    Serial.print(humi);
+    Serial.println("");
+    String hs="Humidity: "+String((int)humi)+" % ";
+    String ts="Temp: "+String((int)tem)+" C ";
+    lcd.setCursor(0, 0);
+    lcd.print(ts);
+    lcd.setCursor(0, 1);
+    lcd.print(hs);
+  
+    static char temp[7];
+    dtostrf(tem, 6, 2, temp);
+    client.publish("get-current-temperature", temp);
+    static char humid[7];
+    dtostrf(humi, 6, 2, humid);
+    client.publish("get-current-humidity", humid);
+    
+    static char res[7];
+    dtostrf(t, 6, 0, res);
+    client.publish("get-light-intensity", res);
+
+    Serial.print("Intensity is: ");
+    Serial.print(t);
+    Serial.println("");
+  }
+  
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! rfid.PICC_IsNewCardPresent())
+      return;
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+      return;
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+          piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+          piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+      Serial.println(F("Your tag is not of type MIFARE Classic."));
+      return;
+  }
+  if (rfid.uid.uidByte[0] != nuidPICC[0] ||
+          rfid.uid.uidByte[1] != nuidPICC[1] ||
+          rfid.uid.uidByte[2] != nuidPICC[2] ||
+          rfid.uid.uidByte[3] != nuidPICC[3] ) {
+      Serial.println(F("A new card has been detected."));
+      // Store NUID into nuidPICC array
+      for (byte i = 0; i < 4; i++) {
+          nuidPICC[i] = rfid.uid.uidByte[i];
+      }
+      Serial.println(F("The NUID tag is:"));
+      Serial.print(F("In hex: "));
+      printHex(rfid.uid.uidByte, rfid.uid.size);
+      Serial.println();
+      Serial.print(F("In dec: "));
+      printDec(rfid.uid.uidByte, rfid.uid.size);
+      Serial.println();
+  }
+  else Serial.println(F("Card read previously."));
+  
+  char str[32] = "";
+  array_to_string(rfid.uid.uidByte, 4, str); // Insert (byte array, length, char array for output)
+  Serial.println(str);                       // Print the output uid string
+
+  client.publish("get-current-tag", str);
+  
+  // Halt PICC
+  rfid.PICC_HaltA();
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
+/**
+    Helper routine to dump a byte array as hex values to Serial.
+*/
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+      Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+      Serial.print(buffer[i], HEX);
+  }
+}
+/**
+    Helper routine to dump a byte array as dec values to Serial.
+*/
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+      Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+      Serial.print(buffer[i], DEC);
+  }
+}
+
+void array_to_string(byte array[], unsigned int len, char buffer[])
+{
+  for (unsigned int i = 0; i < len; i++)
+  {
+    byte nib1 = (array[i] >> 4) & 0x0F;
+    byte nib2 = (array[i] >> 0) & 0x0F;
+    buffer[i * 2 + 0] = nib1 < 0xA ? '0' + nib1 : 'A' + nib1 - 0xA;
+    buffer[i * 2 + 1] = nib2 < 0xA ? '0' + nib2 : 'A' + nib2 - 0xA;
+  }
+  buffer[len * 2] = '\0';
+}
